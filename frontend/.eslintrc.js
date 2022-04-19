69d8ed257e89b7ab3c75597886925eb99ba6bef0
JasonTanz <65846113+JasonTanz@users.noreply.github.com>
2022-04-19 20:46:12 +0800

    feat/initial-setup-done

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,62 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+        "plugin:prettier/recommended",
+        "plugin:jsx-a11y/strict",
+        "prettier",
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: "module",
+    },
+    plugins: [
+        "react",
+        "jsx-a11y",
+        "@typescript-eslint",
+        "unused-imports",
+        "prettier",
+    ],
+    rules: {
+        "react-hooks/exhaustive-deps": "error",
+        "no-var": "error",
+        "brace-style": "error",
+        "prefer-template": "error",
+        radix: "error",
+        "space-before-blocks": "error",
+        "import/prefer-default-export": "off",
+        "unused-imports/no-unused-imports": "error",
+        useTabs: 0,
+        "prettier/prettier": [
+            "error",
+            {
+                endOfLine: "auto",
+            },
+        ],
+    },
+    overrides: [
+        {
+            files: [
+                "**/*.test.js",
+                "**/*.test.jsx",
+                "**/*.test.tsx",
+                "**/*.spec.js",
+                "**/*.spec.jsx",
+                "**/*.spec.tsx",
+            ],
+            env: {
+                jest: true,
+            },
+        },
+    ],
+};
---
+++ frontend/src/utils/theme.ts
@@ -0,0 +1,59 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'Work Sans, sans-serif',
+    body: 'Inter, sans-serif',
+  },
+  fontSizes: {
+    xs: '0.75rem',
+    sm: '0.875rem', // 14px
+    md: '1rem', // 15px
+    lg: '1.125rem',
+    xl: '1.25rem',
+    '2xl': '1.5rem', // 25px
+    '3xl': '1.875rem',
+    '4xl': '2.25rem', //35px
+    '5xl': '3rem',
+    '6xl': '3.75rem',
+    '7xl': '4.5rem',
+    '8xl': '6rem',
+    '9xl': '8rem',
+  },
+  sizes: {
+    'container.xl': '1200px',
+    'container.dashboard': '1350px',
+    'container.form': '550px',
+  },
+  components: {
+    stepperBox: {
+      baseStyle: {
+        py: '20px',
+        borderRadius: '10px',
+        mb: '28px',
+        w: '500px',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        px: '25px',
+      },
+      variants: {
+        normal: {
+          border: '1px solid #FFFFFF',
+          color: '#FFFFFF',
+        },
+        success: {
+          border: '1px solid #4EFF9F',
+          color: '#4EFF9F',
+        },
+        locked: {
+          border: '1px solid #407DC1',
+          color: '#407DC1',
+        },
+      },
+      defaultProps: {
+        variant: 'normal',
+      },
+    },
+  },
+});
+export default theme;
