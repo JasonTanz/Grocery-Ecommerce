120767b245cbdf21e755360116464a0beb6e31c1
JasonTanz <65846113+JasonTanz@users.noreply.github.com>
2022-04-21 14:28:50 +0800

    feat/product-migration-model-done

---
+++ backend/src/models/product.ts
@@ -0,0 +1,43 @@
+import { Table, Column, Model, DataType } from 'sequelize-typescript';
+
+@Table
+export class product extends Model<product> {
+  @Column({
+    type: DataType.UUID,
+    primaryKey: true,
+    defaultValue: DataType.UUIDV4,
+    allowNull: false,
+  })
+  product_id: string;
+
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+  })
+  product_name: string;
+
+  @Column({
+    type: DataType.STRING,
+  })
+  product_brief_intro: string;
+
+  @Column({
+    type: DataType.TEXT('long'),
+  })
+  product_description: string;
+
+  @Column({
+    type: DataType.TEXT('long'),
+  })
+  product_img: string;
+
+  @Column({
+    type: DataType.NUMBER,
+  })
+  product_price: number;
+
+  @Column({
+    type: DataType.NUMBER,
+  })
+  product_qty: number;
+}
