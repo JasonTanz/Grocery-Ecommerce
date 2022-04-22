4770d955b54e27cbfe9e8a9e9d5d742355d01254
JasonTanz <65846113+JasonTanz@users.noreply.github.com>
2022-04-22 15:20:40 +0800

    feat/cartItem-module-done

--- backend/src/cart-item/cart-item.module.ts
+++ backend/src/cart-item/cart-item.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { cartsProviders } from './cartItem.provider';
 import { CartItemService } from './cart-item.service';
+import { CartItemResolver } from './cart-item.resolver';
 
 @Module({
-  providers: [...cartsProviders, CartItemService],
+  providers: [...cartsProviders, CartItemService, CartItemResolver],
   exports: [...cartsProviders],
 })
 export class CartItemModule {}
